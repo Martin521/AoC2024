88bd4001b5ada36ceaea3740e2441ffe540853e2
Martin521 <29605222+Martin521@users.noreply.github.com>
2024-12-03 10:38:51 +0100

    Day03+

--- src/Day03.fs
+++ src/Day03.fs
@@ -11,21 +11,14 @@ let getResults (lines: string list) =
         |> Seq.sumBy (fun m -> int m.Groups[1].Captures[0].Value * int m.Groups[2].Captures[0].Value)
     let result1 = getMulSum input
 
-    let dontIndices = Regex.Matches(input, "(don't)") |> Seq.map _.Index |> Seq.toList
-    let dontSet = Set dontIndices
-    let doIndices =
-        Regex.Matches(input, "(do)") |> Seq.map _.Index |> Seq.filter (dontSet.Contains >> not) |> Seq.toList
-    let rec getSectionsStarted start dos donts =
-        match donts with
-        | [] -> [start, input.Length - 1]
-        | h :: t when h < start -> getSectionsStarted start dos t
-        | h :: t -> (start, donts.Head) :: getSections h dos t
-    and getSections prevEnd dos donts =
-        match dos with
-        | [] -> []
-        | h :: t when h < prevEnd -> getSections prevEnd t donts
-        | h :: t -> getSectionsStarted h t donts
-    let enabledSections = getSectionsStarted 0 doIndices dontIndices |> List.map (fun (s, e) -> input[s..e])
-    let result2 = enabledSections |> List.sumBy getMulSum
+    let rec getEnabledSections (s: string) =
+        match s.IndexOf "don't()" with
+        | -1 -> [s]
+        | i -> s[.. i] :: getEnabledSectionsNext s[i + 7 ..]
+    and getEnabledSectionsNext s =
+        match s.IndexOf "do()" with
+        | -1 -> []
+        | i -> getEnabledSections s[i ..]
+    let result2 = getEnabledSections input |> List.sumBy getMulSum
 
     result1, result2
