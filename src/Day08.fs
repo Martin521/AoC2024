2dcc921f4fc1b11365be52f864e1dc4fd7fd2674
Martin521 <29605222+Martin521@users.noreply.github.com>
2024-12-08 12:53:50 +0100

    Day08+

--- src/Day08.fs
+++ src/Day08.fs
@@ -16,40 +16,41 @@ let testInput =
 ............
 ............"""
 
+type Parts = Part1 | Part2
+
 let getResults (lines: string list) =
     let lines = if useExample then splitLines testInput else lines
     let nr = lines.Length
     let nc = lines.Head.Length
-    let inBounds (r, c) = r >= 0 && r < nr && c >= 0 && c < nc
     let antennas =
         lines
-        |> List.mapi (fun r -> Seq.mapi (fun c a -> a, (r, c)))
-        |> Seq.concat
-        |> Seq.groupBy fst
-        |> Map
-        |> Map.remove '.'
-        |> Map.toList
-        |> List.map (snd >> Seq.map snd >> Set)
-    let rec getAntipodeSet single aSet =
-        if Set.isEmpty aSet then Set.empty else
-        let (rn, cn) = aSet.MinimumElement
-        let rSet = aSet.Remove(rn, cn)
-        let getAntipodes (ra, ca) =
-            let dr, dc = ra - rn, ca - cn
-            if single then
-                Set [
-                    if inBounds (ra + dr, ca + dc) then ra + dr, ca + dc
-                    if inBounds (rn - dr, cn - dc) then rn - dr, cn - dc
-                ]
-            else
-                Set [
-                    let rec getInBounds r c dr dc =
-                        if inBounds (r, c) then (r, c) :: getInBounds (r + dr) (c + dc) dr dc else []
-                    yield! getInBounds ra ca dr dc
-                    yield! getInBounds rn cn -dr -dc
-                ]
-        let nextAntipodes = rSet |> Set.map getAntipodes |> Set.unionMany
-        nextAntipodes + getAntipodeSet single rSet
-    let getCount single = antennas |> List.map (getAntipodeSet single) |> Set.unionMany |> Set.count
+        |> List.mapi (fun r -> Seq.mapi (fun c a -> a, (r, c)) >> Seq.toList)
+        |> List.concat
+        |> List.groupBy fst
+        |> List.filter (fst >> (<>) '.')
+        |> List.map (snd >> List.map snd)
+    let rec getGroupAntipodes dayPart antennaGroup =
+        match antennaGroup with
+        | [] -> Set.empty
+        | (r1, c1) :: groupRemainder ->
+            let getAntipodes (r2, c2) =
+                let dr, dc = r1 - r2, c1 - c2
+                let inBounds (r, c) = r >= 0 && r < nr && c >= 0 && c < nc
+                match dayPart with
+                | Part1 ->
+                    Set [
+                        if inBounds (r1 + dr, c1 + dc) then r1 + dr, c1 + dc
+                        if inBounds (r2 - dr, c2 - dc) then r2 - dr, c2 - dc
+                    ]
+                | Part2 ->
+                    Set [
+                        let rec getInBounds r c dr dc =
+                            if inBounds (r, c) then (r, c) :: getInBounds (r - dr) (c - dc) dr dc else []
+                        yield! getInBounds r1 c1 dr dc
+                        yield! getInBounds r2 c2 -dr -dc
+                    ]
+            let antennaAntipodes = groupRemainder |> List.map getAntipodes |> Set.unionMany
+            antennaAntipodes + getGroupAntipodes dayPart groupRemainder
+    let getCount dayPart = antennas |> List.map (getGroupAntipodes dayPart) |> Set.unionMany |> Set.count
 
-    getCount true, getCount false
+    getCount Part1, getCount Part2
