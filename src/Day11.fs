5b48ee6d140e94d141d2b03311a8640252b1d214
Martin521 <29605222+Martin521@users.noreply.github.com>
2024-12-11 14:42:11 +0100

    Day11

--- src/Day11.fs
+++ src/Day11.fs
@@ -3,9 +3,37 @@
 open AoClib
 
 let testInput =
-    """"""
+    // """0 1 10 99 999"""
+    """125 17"""
 
 let getResults (lines: string list) =
     let lines = if useExample then splitLines testInput else lines
+    let numbers = lines.Head.Split(" ") |> Seq.map int64 |> Seq.toList
+    let rec getDigitsR = function 0L -> [] | n -> n % 10L :: getDigitsR (n / 10L)
+    let rec getNumberR = function [] -> 0L | h::t -> h + 10L * (getNumberR t)
+    let split n =
+        if n = 0L then [1L]
+        else
+            let dsr = getDigitsR n
+            if List.length dsr % 2 = 0 then
+                dsr |> List.splitInto 2 |> List.map getNumberR |> List.rev
+            else [2024L * n]
+    // let rec blink1 ns = function 0 -> ns | k -> blink1 (ns |> List.collect split) (k - 1)
+    let rec blinkC caches ns k =
+        let caches =
+            match caches with
+            | [] -> [Map.empty]
+            | _ -> caches
+        match k with
+        | 0 -> [Map.empty], int64 (List.length ns)
+        | _ ->
+            ((caches, 0L), ns) ||> List.fold (fun (caches, count) n ->
+                match Map.tryFind n caches.Head with
+                | None -> 
+                    let nextCaches, c = blinkC caches.Tail (split n) (k - 1)
+                    Map.add n c caches.Head :: nextCaches, count + c
+                | Some c -> caches, count + c
+            )
+    let blink numbers k = blinkC [] numbers k |> snd
 
-    0, 0
+    blink numbers 25, blink numbers 75
