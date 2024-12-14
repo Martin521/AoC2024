246ec301f9cf6bd6d3903babb43f399840bbd98c
Martin521 <29605222+Martin521@users.noreply.github.com>
2024-12-14 19:34:54 +0100

    Day13

--- src/Day13.fs
+++ src/Day13.fs
@@ -1,11 +1,44 @@
 ﻿module Day13
 
 open AoClib
+open System.Text.RegularExpressions
+
+#nowarn 25
 
 let testInput =
-    """"""
+    """Button A: X+94, Y+34
+Button B: X+22, Y+67
+Prize: X=8400, Y=5400
+
+Button A: X+26, Y+66
+Button B: X+67, Y+21
+Prize: X=12748, Y=12176
+
+Button A: X+17, Y+86
+Button B: X+84, Y+37
+Prize: X=7870, Y=6450
+
+Button A: X+69, Y+23
+Button B: X+27, Y+71
+Prize: X=18641, Y=10279"""
 
 let getResults (lines: string list) =
-    // let lines = splitLines testInput
+    let lines = if useExample then splitLines testInput else lines
+    let regex = Regex(""".+: X[+=](\d+), Y[+=](\d+)""")
+    let getEquation line = regex.Match(line).Groups |> Seq.tail |> Seq.map (_.Value >> int >> bigint) |> Seq.toList
+    let machines = lines |> List.chunkBySize 4 |> List.map (List.take 3 >> List.map getEquation >> List.transpose)
 
-    0, 0
+    let getResult isPart2 =
+        let solve equations =
+            let [x1; x2; X] = List.head equations
+            let [y1; y2; Y] = equations.Tail.Head
+            let X, Y = if isPart2 then X + 10000000000000I, Y + 10000000000000I else X, Y
+            let nom2 = X * y1 - Y * x1
+            let den2 = x2 * y1 - y2 * x1
+            let nom1 = X * y2 - Y * x2
+            let den1 = - den2
+            if nom1 % den1 = 0I && nom2 % den2 = 0I then Some (nom1 / den1, nom2 / den2) else None
+        machines |> List.choose solve |> List.sumBy (fun (n1, n2) -> 3I * n1 + n2)
+
+    getResult false, getResult true
+
--- src/lib.fs
+++ src/lib.fs
@@ -11,3 +11,7 @@ let splitLine (separator: string) (line: string) =
     line.Split separator |> Array.toList
 
 let (|Even|Odd|) input = if input % 2 = 0 then Even else Odd
+
+let rec gcd x y = if y = 0L then x else gcd y (x % y)
+let lcm x y = x * y / gcd x y
+
