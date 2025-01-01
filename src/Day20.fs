2ce0771d40f0585f079118eaad90765f702901cc
Martin521 <29605222+Martin521@users.noreply.github.com>
2025-01-01 16:34:54 +0100

    Day20

--- src/Day20.fs
+++ src/Day20.fs
@@ -3,9 +3,53 @@
 open AoClib
 
 let testInput =
-    """"""
+    """###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S#...#.#.#...#
+#######.#.#.###
+#######.#.#...#
+#######.#.###.#
+###..E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############"""
 
 let getResults (lines: string list, example) =
-    let lines = if example = "1" then splitLines testInput else lines
+    let lines, minGain = if example = "1" then splitLines testInput, 50 else lines, 100
+    let nr, nc = lines.Length, lines.Head.Length
+    let walls = mkCharPosSet '#' lines
+    let tracks = Set(allPositions nr nc) - walls
+    let startPos, endPos = findCharPos 'S' lines, findCharPos 'E' lines
+    let dS, dN, dE, dW = (1, 0), (-1, 0), (0, 1), (0, -1)
+    let steps = [dS; dN; dE; dW]
+    let add (dr, dc) (r, c) = r + dr, c + dc
 
-    0, 0
+    let rec getTrace trace prevPos pos =
+        if pos = endPos then
+            pos :: trace
+        else
+            let nextPos =
+                steps
+                |> List.map (add pos)
+                |> List.filter (fun p -> tracks.Contains p && p <> prevPos)
+                |> List.exactlyOne
+            getTrace (pos :: trace) pos nextPos
+    let trace = getTrace [] startPos startPos
+    let iTrace = trace |> List.mapi (fun i (r, c) -> i, r, c)
+    let getCheatCount maxCheatLength =
+        let rec getCount trace1 trace2 count =
+            match trace1, trace2 with
+            | [], _ -> count
+            | h::t, [] -> getCount t t count
+            | (i1, r1, c1)::t1, (i2, r2, c2)::t2 ->
+                let cheatLen = abs (r1 - r2) + abs (c1 - c2)
+                let ct = if cheatLen <= maxCheatLength && abs (i1 - i2) - cheatLen >= minGain then count + 1 else count
+                getCount trace1 t2 ct
+        getCount iTrace iTrace 0
+
+    getCheatCount 2, getCheatCount 20
--- src/lib.fs
+++ src/lib.fs
@@ -13,6 +13,8 @@ let startsWith (s: string) (x: string) = x.StartsWith s
 
 let list2pair xs = List.head xs, xs.Tail.Head
 
+let findCharPos x = Seq.indexed  >> Seq.find (snd >> Seq.contains x) >> fun(r, row) -> r, Seq.findIndex ((=) x) row
+
 let mkCharMap (lines: string list) = lines |> Seq.mapi (fun r -> Seq.mapi (fun c x -> (r, c), x)) |> Seq.concat |> Map
 
 let mkCharPosSet chr (lines: string list) =
@@ -27,11 +29,14 @@ let allPositions nr nc = [
         for c in 0 .. nc - 1 do
             yield r, c
 ]
+
 let allInnerPositions nr nc = [
     for r in 1 .. nr - 2 do
         for c in 1 .. nc - 2 do
             yield r, c
 ]
+
+let largeInt = System.Int32.MaxValue / 2
 
 let (|Even|Odd|) input = if input % 2 = 0 then Even else Odd
 
